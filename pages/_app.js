7392133f26b066b355c3684c5c109865b9d1f4e9
modbender <rameshmamathayashas@gmail.com>
2020-12-30 17:04:25 +0530

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "assets/scss/globals.scss";
+import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
